2f560b2a604b49b9ee891b756091810de5517346
Rodrigo <rodrigolfranca@gmail.com>
2021-12-21 11:33:11 -0300

    Editando código antigo

--- racing3/script.js
+++ racing3/script.js
@@ -1,4 +1,4 @@
-let count0, count1, count2, vel0, vel1, vel2, voltas, aux = 0;
+let vel0, vel1, vel2, voltas, aux, pontos = 0;
 const lapradio = document.querySelectorAll(".input");
 const min0 = document.getElementById("min0")
 const max0 = document.getElementById("max0")
@@ -16,7 +16,6 @@ const exp1 = document.getElementById("exp1")
 const level1 = document.getElementById("level1")
 const exp2 = document.getElementById("exp2")
 const level2 = document.getElementById("level2")
-
 //carros criados
 let player = [
     {
@@ -28,6 +27,7 @@ let player = [
         speedMax: 0,
         skid: 0
         },
+        voltas: 0,
         EXP: 0,
         level: 1,
     },
@@ -40,6 +40,7 @@ let player = [
         speedMax: 0,
         skid: 0
         },
+        voltas: 0,
         EXP: 0,
         level: 1,
     },
@@ -50,13 +51,13 @@ let player = [
         level: 1,
         speedMin: 0,
         speedMax: 0,
-        skid: 0
+        skid: 0        
         },
+        voltas: 0,
         EXP: 0,
         level: 1,
     }
 ]
-
 //modelos de carro
 let popular = {
     vmaxima: [180 , 200] ,
@@ -73,10 +74,8 @@ let superSport = {
     vminima: [125 , 145] ,
     skid: [1 , 1.75]
 }
-
 //fabricando carros
 function makeCar(id) {
-
     aux = geraRandom(0 , 100);
     if (aux < 60) {
         player[id].car.rarity = "Popular"
@@ -85,7 +84,6 @@ function makeCar(id) {
     } else {
         player[id].car.rarity = "Super Sport"
     }
-
     if ( player[id].car.rarity == "Popular") {        
         player[id].car.speedMin = parseInt(geraRandom(popular.vminima[0] , popular.vminima[1]));
         player[id].car.speedMax = parseInt(geraRandom(popular.vmaxima[0] , popular.vmaxima[1]));
@@ -99,26 +97,26 @@ function makeCar(id) {
         player[id].car.speedMax = parseInt(geraRandom(superSport.vmaxima[0] , superSport.vmaxima[1]));
         player[id].car.skid = geraRandom(superSport.skid[0] , superSport.skid[1]).toFixed(2);
     }
-
     player[id].car.level = 1;
-
     document.getElementById("rarity"+id).innerHTML = "Raridade: "+player[id].car.rarity;
     document.getElementById("min"+id).innerHTML = "Mínima: "+player[id].car.speedMin+"Km/h";
     document.getElementById("max"+id).innerHTML = "Máxima: "+player[id].car.speedMax+"Km/h";
     document.getElementById("derrapagem"+id).innerHTML = "Derrapagem: "+player[id].car.skid+"%";
 }
-
 //contando as voltas
 function vai() {    
     if (lapradio[0].checked) {
+        pontos = [200, 120, 50, 100]
         voltas = 10;
         race();
     }
     if (lapradio[1].checked) {
+        pontos = [220, 130, 75, 115]
         voltas = 70;
         race();
     }
     if (lapradio[2].checked) {
+        pontos = [250, 150, 90, 130]
         voltas = 160;
         race();
     }
@@ -126,9 +124,9 @@ function vai() {
 
 //corrida
 function race(){
-    count0 = 0;
-    count1 = 0;
-    count2 = 0;
+    player[0].voltas = 0;
+    player[1].voltas = 0;
+    player[2].voltas = 0;
     //atualizando valores por level
     player.forEach(elem => {
         if (elem.EXP > 450) {
@@ -157,157 +155,62 @@ function race(){
             level2.innerHTML = "Level: "+elem.level;
         }
     });
-
     //A Corrida
     for (let i=0; i<=voltas-1; i++) {
-
         console.log("Volta: "+(i+1))
-        vel0 = geraRandom(player[0].car.speedMin , player[0].car.speedMax);
-        vel0 = parseInt(vel0 - ((vel0 * player[0].car.skid)/100));
-        console.log("Velocidade do Pedro: "+vel0);
-        vel1 = geraRandom(player[1].car.speedMin , player[1].car.speedMax);
-        vel1 = parseInt(vel1 - ((vel1 * player[1].car.skid)/100));
-        console.log("Velocidade do Juca: "+vel1);
-        vel2 = geraRandom(player[2].car.speedMin , player[2].car.speedMax);
-        vel2 = parseInt(vel2 - ((vel2 * player[2].car.skid)/100));
-        console.log("Velocidade da Edna: "+vel2);
+        vel0 = (geraRandom(player[0].car.speedMin , player[0].car.speedMax)) * (1 - (player[0].car.skid / 100));
+        vel1 = (geraRandom(player[1].car.speedMin , player[1].car.speedMax)) * (1 - (player[1].car.skid / 100));
+        vel2 = (geraRandom(player[2].car.speedMin , player[2].car.speedMax)) * (1 - (player[0].car.skid / 100));
         if (vel0 > vel1 && vel0 > vel2) {
-            count0 = count0 + 1
-            console.log("Volta de Pedro")
+            player[0].voltas = player[0].voltas + 1
         }
         if (vel1 > vel0 && vel1 > vel2) {
-            count1 = count1 + 1
-            console.log("Volta de Juca")
+            player[1].voltas = player[1].voltas + 1
         }
         if (vel2 > vel1 && vel2 > vel0) {
-            count2 = count2 + 1
-            console.log("Volta de Edna")
+            player[2].voltas = player[2].voltas + 1
         }
     }
-
     //anuncio + distribuição de EXP
-    if (voltas == 10) {
-        if (count0 > count1 && count0 > count2){
-            player[0].EXP = player[0].EXP + 200;
-            if (count1 > count2) {
-                player[1].EXP = player[1].EXP + 120;
-                player[2].EXP = player[2].EXP + 50;
-                winner.innerHTML = "1º: Pedro - 2º: Juca - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 120;
-                player[1].EXP = player[1].EXP + 50;
-                winner.innerHTML = "1º: Pedro - 2º: Edna - 3º: Juca"
-            }
-        } else if (count1 > count0 && count1 > count2){            
-            player[1].EXP = player[1].EXP + 200;
-            if (count0 > count2) {
-                player[0].EXP = player[0].EXP + 120;
-                player[2].EXP = player[2].EXP + 50;
-                winner.innerHTML = "1º: Juca - 2º: Pedro - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 120;
-                player[0].EXP = player[0].EXP + 50;
-                winner.innerHTML = "1º: Juca - 2º: Edna - 3º: Pedro"
-            }
-        } else if (count2 > count1 && count2 > count0){            
-            player[2].EXP = player[2].EXP + 200;
-            if (count1 > count0) {
-                player[1].EXP = player[1].EXP + 120;
-                player[0].EXP = player[0].EXP + 50;
-                winner.innerHTML = "1º: Edna - 2º: Juca - 3º: Pedro"
-            } else {
-                player[0].EXP = player[0].EXP + 120;
-                player[1].EXP = player[1].EXP + 50;
-                winner.innerHTML = "1º: Edna - 2º: Pedro - 3º: Juca"
-            }
+    if (player[0].voltas > player[1].voltas && player[0].voltas > player[2].voltas){
+        player[0].EXP = player[0].EXP + pontos[0];
+        if (player[1].voltas > player[2].voltas) {
+            player[1].EXP = player[1].EXP + pontos[1];
+            player[2].EXP = player[2].EXP + pontos[2];
+            winner.innerHTML = "1º: Pedro - 2º: Juca - 3º: Edna"
         } else {
-            winner.innerHTML = "Empate"
-            player[0].EXP = player[0].EXP + 100;
-            player[1].EXP = player[1].EXP + 100;
-            player[2].EXP = player[2].EXP + 100;
+            player[2].EXP = player[2].EXP + pontos[1];
+            player[1].EXP = player[1].EXP + pontos[2];
+            winner.innerHTML = "1º: Pedro - 2º: Edna - 3º: Juca"
         }
-    } else if (voltas == 70) {
-        if (count0 > count1 && count0 > count2){
-            player[0].EXP = player[0].EXP + 220;
-            if (count1 > count2) {
-                player[1].EXP = player[1].EXP + 130;
-                player[2].EXP = player[2].EXP + 75;
-                winner.innerHTML = "1º: Pedro - 2º: Juca - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 130;
-                player[1].EXP = player[1].EXP + 75;
-                winner.innerHTML = "1º: Pedro - 2º: Edna - 3º: Juca"
-            }
-        } else if (count1 > count0 && count1 > count2){
-            player[1].EXP = player[1].EXP + 220;
-            if (count0 > count2) {
-                player[0].EXP = player[0].EXP + 130;
-                player[2].EXP = player[2].EXP + 75;
-                winner.innerHTML = "1º: Juca - 2º: Pedro - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 130;
-                player[0].EXP = player[0].EXP + 75;
-                winner.innerHTML = "1º: Juca - 2º: Edna - 3º: Pedro"
-            }
-        } else if (count2 > count1 && count2 > count0){
-            player[2].EXP = player[2].EXP + 220;
-            if (count1 > count0) {
-                player[1].EXP = player[1].EXP + 130;
-                player[0].EXP = player[0].EXP + 75;
-                winner.innerHTML = "1º: Edna - 2º: Juca - 3º: Pedro"
-            } else {
-                player[0].EXP = player[0].EXP + 130;
-                player[1].EXP = player[1].EXP + 75;
-                winner.innerHTML = "1º: Edna - 2º: Pedro - 3º: Juca"
-            }
+    } else if (player[1].voltas > player[0].voltas && player[1].voltas > player[2].voltas){
+        player[1].EXP = player[1].EXP + pontos[0];
+        if (player[0].voltas > player[2].voltas) {
+            player[0].EXP = player[0].EXP + pontos[1];
+            player[2].EXP = player[2].EXP + pontos[2];
+            winner.innerHTML = "1º: Juca - 2º: Pedro - 3º: Edna"
         } else {
-            winner.innerHTML = "Empate"
-            player[0].EXP = player[0].EXP + 115;
-            player[1].EXP = player[1].EXP + 115;
-            player[2].EXP = player[2].EXP + 115;
+            player[2].EXP = player[2].EXP + pontos[1];
+            player[0].EXP = player[0].EXP + pontos[2];
+            winner.innerHTML = "1º: Juca - 2º: Edna - 3º: Pedro"
+        }
+    } else if (player[2].voltas > player[1].voltas && player[2].voltas > player[0].voltas){
+        player[2].EXP = player[2].EXP + pontos[0];
+        if (player[1].voltas > player[0].voltas) {
+            player[1].EXP = player[1].EXP + pontos[1];
+            player[0].EXP = player[0].EXP + pontos[2];
+            winner.innerHTML = "1º: Edna - 2º: Juca - 3º: Pedro"
+        } else {
+            player[0].EXP = player[0].EXP + pontos[1];
+            player[1].EXP = player[1].EXP + pontos[2];
+            winner.innerHTML = "1º: Edna - 2º: Pedro - 3º: Juca"
         }
     } else {
-        if (count0 > count1 && count0 > count2){
-            player[0].EXP = player[0].EXP + 250;
-            if (count1 > count2) {
-                player[1].EXP = player[1].EXP + 150;
-                player[2].EXP = player[2].EXP + 90;
-                winner.innerHTML = "1º: Pedro - 2º: Juca - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 150;
-                player[1].EXP = player[1].EXP + 90;
-                winner.innerHTML = "1º: Pedro - 2º: Edna - 3º: Juca"
-            }
-        } else if (count1 > count0 && count1 > count2){
-            player[1].EXP = player[1].EXP + 250;
-            if (count0 > count2) {
-                player[0].EXP = player[0].EXP + 150;
-                player[2].EXP = player[2].EXP + 90;
-                winner.innerHTML = "1º: Juca - 2º: Pedro - 3º: Edna"
-            } else {
-                player[2].EXP = player[2].EXP + 150;
-                player[0].EXP = player[0].EXP + 90;
-                winner.innerHTML = "1º: Juca - 2º: Edna - 3º: Pedro"
-            }
-        } else if (count2 > count1 && count2 > count0){
-            player[2].EXP = player[2].EXP + 250;
-            if (count1 > count0) {
-                player[1].EXP = player[1].EXP + 150;
-                player[0].EXP = player[0].EXP + 90;
-                winner.innerHTML = "1º: Edna - 2º: Juca - 3º: Pedro"
-            } else {
-                player[0].EXP = player[0].EXP + 150;
-                player[1].EXP = player[1].EXP + 90;
-                winner.innerHTML = "1º: Edna - 2º: Pedro - 3º: Juca"
-            }
-        } else {
-            winner.innerHTML = "Empate"
-            player[0].EXP = player[0].EXP + 130;
-            player[1].EXP = player[1].EXP + 130;
-            player[2].EXP = player[2].EXP + 130;
-        }
+        winner.innerHTML = "Empate"
+        player[0].EXP = player[0].EXP + pontos[3]
+        player[1].EXP = player[1].EXP + pontos[3]
+        player[2].EXP = player[2].EXP + pontos[3]
     }
-    
     //HUD
     exp0.innerHTML = "EXP: "+player[0].EXP;
     exp1.innerHTML = "EXP: "+player[1].EXP;
