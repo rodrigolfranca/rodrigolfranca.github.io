3d51651bff18d01023cc6c3abbdf876a36a34f4d
Rodrigo <rodrigolfranca@gmail.com>
2022-01-27 11:13:53 -0300

    correção: corrigindo iteração entre objetos

--- recursive/exercicio2.js
+++ recursive/exercicio2.js
@@ -1,7 +1,7 @@
-const matriz = [[1 , 2 , 3], ["Maria", "João", "Pedro", ["José", "Zéquinha"]], [true , false], [ undefined ]];
+const matriz = [[1 , 2 , 3], ["Maria", "João", "Pedro", ["José", "Zéquinha"]], [true , false], { teste: 'teste' }, [ undefined ]];
 
 function mapeador(_dado) {
-    if (typeof _dado === "object" ) {
+    if (typeof _dado === "object" && _dado[0] !== undefined) {
         _dado.forEach(dado => {
             mapeador(dado);            
         });
