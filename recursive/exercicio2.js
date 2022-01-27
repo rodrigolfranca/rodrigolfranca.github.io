d99a426e7e79aa4366eb972c0772e92992992f96
Rodrigo <rodrigolfranca@gmail.com>
2022-01-27 11:15:09 -0300

    adição: comentário sobre última atualização.

--- recursive/exercicio2.js
+++ recursive/exercicio2.js
@@ -1,6 +1,7 @@
 const matriz = [[1 , 2 , 3], ["Maria", "João", "Pedro", ["José", "Zéquinha"]], [true , false], { teste: 'teste' }, [ undefined ]];
 
 function mapeador(_dado) {
+    // _dado[0] !== undefined esta condição é adcionada para que possa ser possivel listar objetos dentro da matriz
     if (typeof _dado === "object" && _dado[0] !== undefined) {
         _dado.forEach(dado => {
             mapeador(dado);            
