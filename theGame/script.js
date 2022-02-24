97c8fac6c52a526c0e42393ce886d2ff5465148e
Rodrigo <rodrigolfranca@gmail.com>
2022-02-24 08:47:51 -0300

    add: alterEgo, reset-btn

--- theGame/script.js
+++ theGame/script.js
@@ -1,12 +1,12 @@
 class Hero {
-    constructor(name, type, imagem, st, agi, int, atkText, special) {
+    constructor(name, type, reference, str, agi, int, atkText, special) {
     this.name = name                      //Nome
     this.type = type                      //Tipo
-    this.imagem = imagem                  //identificador
-    this.st = st		                  //Força
+    this.reference = reference            //Referencia para audio e imagem
+    this.str = str		                  //Força
     this.agi = agi	                      //Agilidade
     this.int = int	                      //Inteligência
-    this.hp = this.st * 8                 //Pontos de Vida atual
+    this.hp = this.str * 8                //Pontos de Vida atual
     this.hpmax = this.hp                  //Pontos de Vida Máximo   
     this.def = parseInt(this.agi * 0.2);  //Defesa
     this.atkPower = 5                     //Ataque base
@@ -29,7 +29,7 @@ class Hero {
         break;
 
         case 2:
-        this.atkPower += this.st /2
+        this.atkPower += this.str /2
         break;
 
         case 3:
@@ -43,8 +43,11 @@ class Hero {
 let defStatus = 0
 let playerDanger = 0;
 let cpuDanger = 0;
+let playerAlter = 0;
+let cpuAlter = 0;   
 let p1 = 0;
 let p2 = 0;
+let victoryAudio;
 const selectCharacterAudio = new Audio("./audio/selectCharacter.mp3");
 const hoverCharacter = new Audio("./audio/hoverCharacter.mp3");
 const oneSelect = new Audio("./audio/oneSelect.mp3");
@@ -66,7 +69,15 @@ chooseHero = [
     Florinda = new Hero("Dona Florinda", 2, 'florinda', 20, 12, 16, 'Dona Florinda virou o tapão em ', "Dona Florinda usa seus dotes de mulher indefesa e destroi seu oponente em tapas"),
     Barriga = new Hero("Seu Barriga", 2, 'barriga', 26, 10, 12, 'Seu barriga deu uma barrigada em ', "Seu Barriga cobra todos os alugueis atrasados de seu oponente, com juros."),
     Professor = new Hero("Prof Girafales", 3, 'professor', 18, 10, 22, 'Professor Girafales deu uma lição de moral em ', "Professor Girafales usa todas as regras da aritmetica pra destruir seu oponente"),
-    Clotilde = new Hero("Dna Clotilde", 3, "clotilde", 8, 10, 28, 'Dona Clotilde enfeitiçou ', "Dona Clotilde invoca o proprio Satanás contra seu oponente")    
+    Clotilde = new Hero("Dona Clotilde", 3, "clotilde", 8, 10, 28, 'Dona Clotilde da um pirulito envenenado para ', "Dona Clotilde invoca o proprio Satanás contra seu oponente"),
+    Chapolim = new Hero("Chapolim Colorado", 1, 'chapolim', 30, 20, 15, "Chapolim deu um golpe com sua Marreta Bionica em ", ""),
+    Alma = new Hero("Alma Negra", 1, 'alma', 30, 20, 15, "Alma Negra puxa seu sabre e golpeia ", "1"),
+    Quase = new Hero("Quase Nada", 1, 'quase', 25, 25, 20, "Quase Nada mira e atira em ", "1"),
+    Baratuxa = new Hero("Bruxa Baratuxa", 1, 'baratuxa', 30, 20, 15, "Bruxa Baratuxa lança feitiço em ", "1"),
+    Rosa = new Hero("Rosa Rumorosa", 1, 'rosa', 30, 20, 15, "Rosa, a Rumorosa empresta a arma de seu pai e atira em ", "1"),
+    Bruxa = new Hero("Bruxa do 71", 1, 'bruxa', 30, 20, 15, "A Bruxa do 71 lança um feitiço em ", "1"),
+    Poucas = new Hero("Poucas Trancas", 1, 'poucas', 30, 20, 15, "Poucas Trancas atira em ", ""),
+    Almôndega = new Hero("Almôndega", 1, 'almondega', 30, 20, 15, "Utilizando seu disfarce, Almondega se aproxima e golpeia ", "")
 ]
 
 //  Função para ataques normais
@@ -99,8 +110,7 @@ function ataque(atacante , defensor) {
     }
 
     if (atacante === p2) {
-        $("#oneDef").prop("disabled", false);
-        $('#oneAtk').prop("disabled", false);
+        toggleButtons();
     }
 
     mostrarLutadores();
@@ -115,8 +125,7 @@ function defesa(jogador) {
     defStatus = 1;
     log( `${jogador.name} se prepara para receber um ataque, sua defesa dobra!\n` , jogador );
     if (jogador === p2) {
-        $("#oneDef").prop("disabled", false);
-        $('#oneAtk').prop("disabled", false);
+        toggleButtons();
     }
 
 }
@@ -124,7 +133,7 @@ function defesa(jogador) {
 //  Função de ataque especial
 function special(atacante, defensor) {
 
-    let url = `./audio/${atacante.imagem}Special.mp3`
+    let url = `./audio/${atacante.reference}Special.mp3`
     let specialAudio = new Audio(url);
     specialAudio.play();
 
@@ -139,10 +148,8 @@ function special(atacante, defensor) {
         checaVidas();
         attacked.play();
         if (atacante === p2) {
-            $('#playerOne').effect('shake')
-            cpuDanger = 2;
-            $("#oneDef").prop("disabled", false);
-            $('#oneAtk').prop("disabled", false);
+            $('#playerOne').effect('shake')            
+            toggleButtons();
         } else {
             $('#playerTwo').effect('shake');
             if (p1.hp>0 && p2.hp>0) enemyAction();
@@ -152,15 +159,77 @@ function special(atacante, defensor) {
 
 }
 
+//  Função de troca de personagem (alter ego)
+function alterEgo(player) {
+    if (player === p1) {
+
+        //  Player 1
+        $('#playerOne').hide('explode');
+        p1.hp = p1.hpmax;
+        p1 = selectAlter(p1);
+        p1 = selected(p1);
+        p1.hp = p1.hp / 2;
+        $('#dangerAtk').css('display', 'none');
+        $('#alterEgo-btn').css('display', 'none');
+        playerDanger = 2;
+        playerAlter = 1;
+        mostrarLutadores();
+        $('#playerOne').fadeIn('slow');
+        let comes = new Audio(`./audio/${p1.reference}Comes.mp3`);
+        comes.play();
+        comes.onended = function() {
+            if (cpuAlter !== 2) {
+                let calc = geraRandom(1, 11);
+                console.log(calc);  
+                (calc >= 3)? alterEgo(p2) : enemyAction();
+            } else {
+                enemyAction();
+            }
+        }
+        
+        
+
+    } else {
+
+        //  CPU
+        $('#playerTwo').effect('explode');
+        p2.hp = p2.hpmax;
+        p2 = selectAlter(p2);
+        p2 = selected(p2);
+        p2.hp = p2.hp / 2;
+        cpuDanger = 2;
+        cpuAlter = 2;
+        mostrarLutadores();
+        $('#playerTwo').fadeIn('slow');
+        let comes = new Audio(`./audio/${p2.reference}Comes.mp3`);
+        comes.play();
+        comes.onended = function() {
+            if (playerAlter !== 1) {
+                let calc = geraRandom(1, 11);
+                console.log(calc);
+                (calc >= 3)? alterEgo(p1) : toggleButtons();
+            } else {
+                toggleButtons();
+            }
+        }
+
+    }
+}
+
 //  Função que sorteia as ações da CPU
 function enemyAction(){
 
     setTimeout(() => {
 
-        let action = geraRandom(1, 10);
-        if (cpuDanger === 1) { 
+        let action = geraRandom(1, 11);
+        if (cpuAlter === 1) {
+
+            alterEgo(p2);            
+
+        } else if (cpuDanger === 1) { 
 
             special(p2, p1);
+            cpuDanger = 2;
 
         } else if (action > 4 || defStatus === 1) {
 
@@ -196,6 +265,28 @@ function selected(hero) {
 
     })[0] 
 
+}
+
+//  Função para selecionar referencia de Alter Ego
+function selectAlter(player) {
+    switch (player.reference) {
+        case 'chaves':
+            return 'Chapolim Colorado'
+        case 'chiquinha':
+            return 'Bruxa Baratuxa'
+        case 'quico':
+            return 'Quase Nada'
+        case 'madruga':
+            return 'Alma Negra'
+        case 'barriga':
+            return 'Almôndega'
+        case 'professor':
+            return 'Poucas Trancas'
+        case 'florinda':
+            return 'Rosa Rumorosa'
+        case 'clotilde':
+            return 'Bruxa do 71'
+    }
 }
 
 //  Função para enviar os logs para a textarea
@@ -275,8 +366,9 @@ $(".fighter").click(function(){
             $("#fight-btn").css("color", "yellow");
         } else {
             erro.play();
-        }
-        
+        }        
+    } else {
+        erro.play();
     }
 });
 
@@ -288,11 +380,17 @@ $("#fight-btn").click(function(){
         $('#seletor-painel').hide();
         selectCharacterAudio.pause();
         $('main').fadeIn(1000);
+        $("#oneDef").prop("disabled", false);
+        $('#oneAtk').prop("disabled", false);
+        $('#logText').val("");
         $('main').css('display', 'flex');
+        $(`#${p1.reference}`).css("filter", "grayscale(0%)");
+        $(`#${p2.reference}`).css("filter", "grayscale(0%)");
         mostrarLutadores();
 
         battleTheme.loop = true;
-        battleTheme.play();    
+        battleTheme.currentTime = 0;
+        battleTheme.play();
     }
 
 })
@@ -301,18 +399,18 @@ $("#fight-btn").click(function(){
 function mostrarLutadores(){
 
     //  Player 1
-    $('#img-one').attr("src", `./images/${p1.imagem}-large-right.png`);
+    $('#img-one').attr("src", `./images/${p1.reference}-large-right.png`);
     $('#oneName').text(`${p1.name}`);
-    $('#oneSTR').text(`${p1.st}`);
+    $('#oneSTR').text(`${p1.str}`);
     $('#oneAGI').text(`${p1.agi}`);
     $('#oneINT').text(`${p1.int}`);
     let porcentagemOne = (p1.hp * 100) / p1.hpmax
     $('#oneVida').css('width', `${porcentagemOne * 2}px`);
 
     //  CPU
-    $('#img-two').attr("src", `./images/${p2.imagem}-large-left.png`);
+    $('#img-two').attr("src", `./images/${p2.reference}-large-left.png`);
     $('#twoName').text(`${p2.name}`);
-    $('#twoSTR').text(`${p2.st}`);
+    $('#twoSTR').text(`${p2.str}`);
     $('#twoAGI').text(`${p2.agi}`);
     $('#twoINT').text(`${p2.int}`);
     let porcentagemTwo = (p2.hp * 100) / p2.hpmax
@@ -320,11 +418,29 @@ function mostrarLutadores(){
 
 }
 
+// Função para desabilitar/habilitar botoes do jogador
+function toggleButtons() {
+    if ($("#oneAtk").prop("disabled") === true) {
+
+        $("#oneDef").prop("disabled", false);
+        $('#oneAtk').prop("disabled", false);
+        $('#dangerAtk').prop('disabled', false);
+        $('#alterEgo-btn').prop('disabled', false);
+
+    } else {
+
+        $("#oneDef").prop("disabled", true);
+        $('#oneAtk').prop("disabled", true);
+        $('#dangerAtk').prop('disabled', true);
+        $('#alterEgo-btn').prop('disabled', true);
+
+    }
+}
+
 //  Botoes durante a luta
 $('#oneAtk').click(function(){
 
-    $("#oneDef").prop("disabled", true);
-    $('#oneAtk').prop("disabled", true);
+    toggleButtons();
     ataque(p1, p2);    
     if (p1.hp>0 && p2.hp>0) enemyAction();
 
@@ -332,8 +448,7 @@ $('#oneAtk').click(function(){
 
 $('#oneDef').click(function(){
 
-    $("#oneDef").prop("disabled", true);
-    $('#oneAtk').prop("disabled", true);
+    toggleButtons();
     defesa(p1);
     if (p1.hp>0 && p2.hp>0) enemyAction();
 
@@ -341,9 +456,15 @@ $('#oneDef').click(function(){
 
 $("#dangerAtk").click(function(){
 
-    $("#oneDef").prop("disabled", true);
-    $('#oneAtk').prop("disabled", true);
+    toggleButtons();
     special(p1, p2);    
+
+})
+
+$('#alterEgo-btn').click(function(){
+
+    toggleButtons();
+    alterEgo(p1);
 
 })
 
@@ -364,7 +485,23 @@ function checaVidas(){
         cpuDanger = 1;
 
     }
-    
+
+    //  Alter Ego enabler
+    if ( ((p1.hp * 100)/ p1.hpmax) <= 10 && playerAlter === 0 && p1.hp >= 1) {
+
+        danger.play();
+        playerAlter = 1;
+        $('#alterEgo-btn').css('display', 'block');
+
+    }
+    if ( ((p2.hp * 100)/ p2.hpmax) <= 10 && cpuAlter === 0 && p2.hp >= 1 ) {
+
+        danger.play();
+        cpuAlter = 1;
+
+    }
+
+
     //  End game
     if (p1.hp < 1) {
 
@@ -373,10 +510,14 @@ function checaVidas(){
         $("#victory").fadeIn("slow");
         $('#victory').css("display","flex");
         $('#victoryText').text(`${p2.name} Wins`);
+        $('#victoryText').css(`color`, 'red');
+        $('#reset-btn').css('color', 'red');
+
         end.play();        
         end.onended = () => {
-            let url = `./audio/${p2.imagem}Victory.mp3`
-            new Audio(url).play();            
+            let url = `./audio/${p2.reference}Victory.mp3`
+            victoryAudio = new Audio(url);
+            victoryAudio.play();
         }
 
 
@@ -391,9 +532,36 @@ function checaVidas(){
         $('#victoryText').text(`${p1.name} Wins`);
         end.play();
         end.onended = () => {
-            let url = `./audio/${p1.imagem}Victory.mp3`
-            new Audio(url).play();           
+            let url = `./audio/${p1.reference}Victory.mp3`
+            victoryAudio = new Audio(url);
+            victoryAudio.play();
         }
 
     }
 }
+
+$('#reset-btn').click(function(){
+
+    $('#victory').fadeOut();
+    $('#seletor-painel').fadeIn();
+    $('#fight-btn').css('color', 'blue');
+    $('#fight-btn').text('PLAYER ONE');
+    $('#alterEgo-btn').css('display', 'none');
+    $('#dangerAtk').css('display', 'none');
+    
+
+    p1.hp = p1.hpmax;
+    p2.hp = p2.hpmax;    
+    cpuAlter = 0;
+    cpuDanger = 0;
+    playerAlter = 0;
+    playerDanger = 0;
+    p1 = 0;
+    p2 = 0;
+
+    victoryAudio.pause();
+    selectCharacterAudio.currentTime = 0;
+    selectCharacterAudio.loop = true;    
+    selectCharacterAudio.play();
+
+})
